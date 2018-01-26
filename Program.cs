08a41ec05ee8f5fdc247620384a368576267397d
DanE <edwardsdan001@gmail.com>
2018-01-26 15:42:09 -0500

    Added methods for print and validate

--- Program.cs
+++ Program.cs
@@ -11,59 +11,73 @@ namespace Lab6
     {
         public static string ValidateInput ()
         {
+            bool loop = true;
             Console.WriteLine("Enter a word or phrase you want to translate!");
             string x = Console.ReadLine();
-            if (x.Length < 1)
+            while (loop == true)
             {
-                Console.WriteLine("Sorry! Try again!");
-                ValidateInput();
+                if (x.Length < 1)
+                {
+                    Console.WriteLine("Sorry! Try again!");
+                    x = Console.ReadLine();
+                }
+                else
+                {
+                    loop = false;
+                }
             }
-                return x;
+            return x;
         }
-        static void Main(string[] args)
-        {
 
+        public static void Print(string[] x)
+        {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
-            string[] Test = ValidateInput().Split(' ');
-
-            for(int i = 0; i<Test.Length; i++)
+            for (int i = 0; i < x.Length; i++)
             {
-                if (Regex.IsMatch(Test[i], @"^[a-zA-Z\']+$") == true)
+                if (Regex.IsMatch(x[i], @"^[a-zA-Z\']+$") == true)
                 {
-                    if (Test[i].IndexOfAny(vowels) == 0)
+                    if (x[i].IndexOfAny(vowels) == 0)
                     {
-                        Console.Write(Test[i] + "way ");
+                        Console.Write(x[i] + "way ");
                         continue;
                     }
                     else
                     {
-                        int vowelIndex = Test[i].IndexOfAny(vowels);
-                        string toAppend = Test[i].Substring(0, vowelIndex);
-                        Console.Write(Test[i].Remove(0, vowelIndex) + toAppend + "ay ");
+                        int vowelIndex = x[i].IndexOfAny(vowels);
+                        string toAppend = x[i].Substring(0, vowelIndex);
+                        Console.Write(x[i].Remove(0, vowelIndex) + toAppend + "ay ");
                         continue;
                     }
                 }
-                if (Regex.IsMatch(Test[i], @"^[a-zA-Z\u0021-\u0040]+$") == true)
+                if (Regex.IsMatch(x[i], @"^[a-zA-Z\u0021-\u0040]+$") == true)
                 {
-                    Console.Write(Test[i] + " ");
+                    Console.Write(x[i] + " ");
                 }
             }
-            Console.WriteLine();
+        }
 
-
-
-
-            Console.WriteLine("Translate another word or phrase? (y/n)");
-            string response = Console.ReadLine();
-            if (string.Compare(response, "y", true) == 0)
+        static void Main(string[] args)
+        {
+            bool loop = true;
+            while (loop == true)
             {
-                Console.Clear();
-                Main(args);
-            }
-            else
-            {
-                Console.WriteLine("Goodbye!");
+                string[] Test = ValidateInput().Split(' ');
+                Print(Test);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Translate another word or phrase? (y/n)");
+                string response = Console.ReadLine();
+                if (string.Compare(response, "y", true) == 0)
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                    loop = false;
+                }
             }
         }
     }
